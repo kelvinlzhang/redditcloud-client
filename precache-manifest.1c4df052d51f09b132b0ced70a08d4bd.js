c2603a4f50a23114febb7e06d4aca77f66228739
Kelvin Zhang <k.zhang661@gmail.com>
2018-11-30 16:52:24 -0800

    Updates

---
+++ precache-manifest.1c4df052d51f09b132b0ced70a08d4bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba0ee939b3969e367c0",
+    "url": "/reddit-cloud/static/css/main.c17d274d.chunk.css"
+  },
+  {
+    "revision": "6ba0ee939b3969e367c0",
+    "url": "/reddit-cloud/static/js/main.6ba0ee93.chunk.js"
+  },
+  {
+    "revision": "070c1cbe16f174b34faa",
+    "url": "/reddit-cloud/static/css/1.3100580a.chunk.css"
+  },
+  {
+    "revision": "070c1cbe16f174b34faa",
+    "url": "/reddit-cloud/static/js/1.070c1cbe.chunk.js"
+  },
+  {
+    "revision": "acfb91ec8a3d8f7eaedf",
+    "url": "/reddit-cloud/static/js/runtime~main.acfb91ec.js"
+  },
+  {
+    "revision": "f0ee8d3b930cef28da02e7f00de975a5",
+    "url": "/reddit-cloud/index.html"
+  }
+];
