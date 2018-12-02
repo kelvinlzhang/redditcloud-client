75f4b82eb193a2646cfeadf99ca57c1abba25a56
kzhangg <zhangkaren@g.ucla.edu>
2018-12-02 13:26:58 -0800

    adding in testing-- SeleniumIDE file and Selenium Web Browser Automation (WIP) .. also Chrome and Firefox webdriver

---
+++ src/selen_test.js
@@ -0,0 +1,14 @@
+var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
+
+var driver = new webdriver.Builder().forBrowser('firefox').build();
+driver.get('http://localhost:3000/');
+
+
+//inputs subreddit into field
+driver.findElement(By.tagName('input')).sendKeys('nba');
+
+//select date range
+// driver.findElement(By.className('input')).sendKeys('12/2/2018 12:00 AM');
+driver.findElement(By.className('react-datepicker-ignore-onclickoutside')).sendKeys('12/2/2018 12:10 AM');
+//submit
